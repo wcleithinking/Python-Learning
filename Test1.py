0b620791fe9d62d69e67c08c07566acf3fd22798
wcleithinking <wcleithinking@gmail.com>
2018-04-17 23:50:31 +0800

    add Test1

---
+++ Test1.py
@@ -0,0 +1,11 @@
+n,m,k = input().split(' ')
+n = int(n)
+m = int(m)
+k = int(k)
+list = []
+for i in range(n):
+    for j in range(m):
+        list.append((i+1)*(j+1))
+
+list.sort()
+print(list[k-1])
