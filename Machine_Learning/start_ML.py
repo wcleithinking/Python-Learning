802b0876b2494c352014ef8f1c8e3ac31fba5359
wcleithinking <wcleithinking@gmail.com>
2018-09-28 11:20:24 +0800

    rename linearfit as start_ML

--- Machine_Learning/start_ML.py
+++ Machine_Learning/start_ML.py
@@ -3,21 +3,19 @@
 # *20180815
  
 import numpy as np
+import matplotlib.pyplot as plt
+
 #原始数据
-X=[ 1 ,2  ,3 ,4 ,5 ,6]
+X=[ 1, 2, 3, 4, 5, 6]
 Y=[ 2.6 ,3.4 ,4.7 ,5.5 ,6.47 ,7.8]
  
 #用一次多项式拟合，相当于线性拟合
 z1 = np.polyfit(X, Y, 1)
 p1 = np.poly1d(z1)
-print (z1)  #[ 1.          1.49333333]
-print (p1)  # 1 x + 1.493
-
+print (z1)
+print (p1)
 
 #作图显示
-import matplotlib.pyplot as plt
-import numpy as np
-
 x = np.arange(1, 7)
 y = z1[0] * x + z1[1]
 plt.figure()
