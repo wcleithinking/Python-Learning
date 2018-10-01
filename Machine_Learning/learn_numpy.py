11744382988ce324e53e88b39dc731d19d87b900
wcleithinking <wcleithinking@gmail.com>
2018-10-01 19:13:30 +0800

    indexing

--- Machine_Learning/learn_numpy.py
+++ Machine_Learning/learn_numpy.py
@@ -68,3 +68,39 @@ print(b)
 print(a+b)
 
 # Indexing of ndarray
+arr = np.arange(10)
+print(arr)
+print(arr[5])
+print(arr[5:8])
+arr[5:8]=12
+print(arr)
+arr_slice = arr[5:8]
+print(arr_slice)
+arr_slice[1] = 12345
+print(arr)
+arr_slice[:] = 64
+print(arr)
+arr_slice = arr[5:8].copy()
+print(arr_slice)
+arr_slice[1] = 12345
+print(arr)
+arr_slice[:] = 64
+print(arr)
+arr2d = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(arr2d[2])
+print(arr2d[0][2])
+print(arr2d[0,2])
+arr3d = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print(arr3d)
+print(arr3d[0])
+old_values = arr3d[0].copy()    # backup arr3d[0]
+arr3d[0] = 42
+print(arr3d)
+print('==========分割线==========')
+arr3d[0] = old_values
+print(arr3d)
+print(arr3d[1,0])
+print('==========分割线==========')
+x = arr3d[1]
+print(x)
+print(x[0])
