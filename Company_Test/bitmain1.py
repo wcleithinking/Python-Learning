555aad107f78a796f6eb1c539e1354d61832306a
wcleithinking <wcleithinking@gmail.com>
2018-10-26 11:02:12 +0800

    modified grad_version2

--- Company_Test/bitmain1.py
+++ Company_Test/bitmain1.py
@@ -1,10 +1,8 @@
-import random
 import numpy as np
 import sklearn
 from sklearn.datasets.samples_generator import make_regression
-import matplotlib.pyplot as plt
 from scipy import stats
-import pylab
+import matplotlib.pyplot as plt
 
 
 def gradient_descent_version1(alpha, X, Y, batch_size, tol=0.0001, max_iter=1000):
@@ -41,11 +39,11 @@ def gradient_descent_version1(alpha, X, Y, batch_size, tol=0.0001, max_iter=1000
             0.5 * sum([(Y[i] - theta0[-1]*X[i] - theta1[-1])**2 for i in range(n)]))
 
         if (abs(J[-2] - J[-1]) <= tol):
-            print("Converged, iterations: ", iter, "!!!")
+            print("Version   1: Converged, iterations: ", iter, "!!!")
             converged = True
 
         if iter == max_iter:
-            print("Max iterations reached!!!")
+            print("Version   1: Max iterations reached!!!")
             converged = True
 
     return theta0, theta1, J, iter
@@ -56,11 +54,11 @@ def gradient_descent_version2(alpha, X, Y, batch_size, tol=0.0001, max_iter=1000
     theta = []
     J = []
     # number of samples
-    n = len(X)
+    n = X.shape[0]
     # initial theta
     theta.append(np.ones(2))
     # initial total error J(n)
-    J.append(0.5 * np.sum((Y - np.dot(X, theta))**2))
+    J.append(0.5 * np.sum((Y - np.dot(X, theta[-1])**2)))
 
     # Iterate Loop
     converged = False
@@ -70,21 +68,21 @@ def gradient_descent_version2(alpha, X, Y, batch_size, tol=0.0001, max_iter=1000
         # generate the indexes for the batch sample
         subIndex = np.random.choice(range(n), batch_size)
         # generate the batch sample
-        subX = [X[j] for j in subIndex]
-        subY = [Y[j] for j in subIndex]
+        subX = np.array([X[j] for j in subIndex])
+        subY = np.array([Y[j] for j in subIndex])
         # compute the gradient of batch cost J(m), where m<=n
-        grad = np.dot(subX.transpose(), subY - np.dot(subX, theta))
+        grad = np.dot(-subX.transpose(), subY - np.dot(subX, theta[-1]))
         # update theta
-        theta.append(theta - alpha * grad)
+        theta.append(theta[-1] - alpha * grad)
         # current totla error
-        J.append(0.5 * np.sum((Y - np.dot(X, theta))**2))
+        J.append(0.5 * np.sum((Y - np.dot(X, theta[-1]))**2))
 
         if (abs(J[-2] - J[-1]) <= tol):
-            print("Converged, iterations: ", iter, "!!!")
+            print("Version   2: Converged, iterations: ", iter, "!!!")
             converged = True
 
         if iter == max_iter:
-            print("Max iterations reached!!!")
+            print("Version   2: Max iterations reached!!!")
             converged = True
     return theta, J, iter
 
@@ -99,29 +97,51 @@ if __name__ == '__main__':
     tol = 0.01      # convergence criteria
     max_iter = 1000  # max iterations
     # calculate
-    theta0, theta1, J, iter = gradient_descent_version1(
+    theta0, theta1, J_v1, iter_v1 = gradient_descent_version1(
         alpha, X, Y, batch_size, tol, max_iter)
+    Xnew = np.c_[X, np.ones(X.shape[0])]
+    theta, J_v2, iter_v2 = gradient_descent_version2(
+        alpha, Xnew, Y, batch_size, tol, max_iter)
     # print results
-    print("theta0 = {}\ttheta1 = {}".format(theta0[-1], theta1[-1]))
+    print("Version   1: theta0 = {}\t theta1 = {}".format(
+        float(theta0[-1]), float(theta1[-1])))
+    print("Version   2: theta0 = {}\t theta1 = {}".format(
+        theta[-1][0], theta[-1][1]))
     slope, intercept, r_value, p_value, slope_std_error = stats.linregress(
         X[:, 0], Y)
-    print("slope = {}\tintercept = {}".format(slope, intercept))
+    print("Scipy Stats: theta0 = {}\t theta1 = {}".format(slope, intercept))
 
     # plot total cost
     plt.figure()
-    plt.plot(range(iter+1), J)
-    plt.xlabel('Iter')
+    plt.plot(range(iter_v1+1), J_v1, linewidth=1, label='Version 1')
+    plt.plot(range(iter_v2+1), J_v2, linewidth=2, label='Version 2')
+    plt.legend()
+    plt.xlabel('Iterations')
     plt.title('Total Cost')
     # plot theta
     plt.figure()
-    plt.plot(range(iter+1), theta0)
-    plt.plot(range(iter+1), theta1)
-    plt.xlabel('Iter')
+    plt.plot(range(iter_v1+1), theta0, color='blue',
+             linewidth=1, label=r'$\hat{\theta}_0$ in Version 1')
+    plt.plot(range(iter_v2+1), [subtheta[0] for subtheta in theta],
+             color='yellow', linewidth=2, label=r'$\hat{\theta}_0$ in Version 2')
+    plt.plot(range(iter_v1+1), theta1, color='green',
+             linewidth=1, label=r'$\hat{\theta}_1$ in Version 1')
+    plt.plot(range(iter_v2+1), [subtheta[1] for subtheta in theta],
+             color='magenta', linewidth=2, label=r'$\hat{\theta}_1$ in Version 2')
+    plt.legend()
+    plt.xlabel('Iterations')
     plt.title('Estimated Parameters')
     # plot original points and prediction
     plt.figure()
-    Y_Predict = [theta0[-1]*X[i]+theta1[-1] for i in range(len(X))]
-    plt.plot(X, Y, 'o')
-    plt.plot(X, Y_Predict, 'k-')
+    Y_Predict_v1 = [theta0[-1]*X[i]+theta1[-1] for i in range(len(X))]
+    Y_Predict_v2 = [theta[-1][0]*X[i]+theta[-1][1] for i in range(len(X))]
+    plt.plot(X, Y, 'o', label='Samples')
+    plt.plot(X, Y_Predict_v1, 'b-', label='Version 1')
+    plt.plot(X, Y_Predict_v2, 'y-', label='Version 2')
+    plt.legend()
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Regression Results')
+
+    # Show the figures
     plt.show()
-    
